b3948fd91fd8586c2a1adfbf29cd3402cc705bbf
Rafael Garcia <rafael.dacunhagarcia@icloud.com>
2019-11-15 14:47:17 +0100

    boi

--- app/src/main/java/com/example/labo3_sym/NFCActivity.java
+++ app/src/main/java/com/example/labo3_sym/NFCActivity.java
@@ -1,9 +1,12 @@
 package com.example.labo3_sym;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +19,19 @@ import com.example.labo3_sym.R;
 
 public class NFCActivity extends AppCompatActivity {
 
+    private Button btn_login = null;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
 
+        this.btn_login = findViewById(R.id.btn_login);
 
+        /* Lorsque l'on clique sur un des boutons, lance l'activité correspondante */
+
+        btn_login.setOnClickListener((v) -> {
+            Intent intent = new Intent(NFCActivity.this, NFC_AFTER_LOGIN_ACTIVITY.class);
+            startActivity(intent);
+        });
     }
 }
