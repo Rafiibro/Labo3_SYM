f0abfde763639a0aa4d264c5a2ad6584b806bebc
Rafael Garcia <rafael.dacunhagarcia@icloud.com>
2019-11-15 14:01:35 +0100

    boi

---
+++ app/src/main/java/com/example/labo3_sym/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.labo3_sym;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Button;
+
+import com.example.labo3_sym.R;
+import com.example.labo3_sym.CodesBarresActivity;
+import com.example.labo3_sym.NFCActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_codes_barres = null;
+    private Button btn_nfc = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Active le layout de main activity */
+        setContentView(R.layout.activity_main);
+
+        /* Indique la policy des threads */
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        /* Initialisation des boutons */
+        this.btn_codes_barres = findViewById(R.id.button2);
+        this.btn_nfc = findViewById(R.id.button3);
+
+        /* Lorsque l'on clique sur un des boutons, lance l'activité correspondante */
+
+        btn_codes_barres.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, CodesBarresActivity.class);
+            startActivity(intent);
+        });
+
+        btn_nfc.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, NFCActivity.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/labo3_sym/NFCActivity.java
@@ -0,0 +1,25 @@
+package com.example.labo3_sym;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.example.labo3_sym.R;
+
+//import com.karumi.dexter.Dexter;
+//import com.karumi.dexter.listener.single.BasePermissionListener;
+
+public class NFCActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+
+
+    }
+}
